ed02c3db876f940897e37c934d311c1f56d62679
developing-ar <noelis.roy@gmail.com>
2023-05-05 14:41:33 +0800

    added code and and data sets for ques and ans

--- QB/questions.c
+++ QB/questions.c
@@ -10,21 +10,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 
 /*
 * Returns a list of mcq question ids
 */
-int*questions_mcq(char prog_lang, int num){
-    int*ids=malloc(num*sizeof(int));
+int*questions_mcq(char prog_lang, int mcq, int cod){
+    int ids[mcq+cod];
 
-    FILE *py_fp = fopen("questionset_py.csv","r");
-    FILE *c_fp = fopen("questionset_c.csv","r");
+    FILE *fp = fopen("questionset.csv","r");
 
     char line[500]; //no line is >499 chars
     int i = 0;
+    int j = 0;
 
     //for both programming languages
-    FILE *fp;
+
+    while(i<mcq && j<cod){
+        q_id = rand()%17;
+    }
+
     if(prog_lang = 'a'){
         while (i<num)
         {
@@ -64,7 +69,61 @@ int*questions_mcq(char prog_lang, int num){
         int *q_id;
         char *q_type;
         char buff[200];
-        while(fgets(line, sizeof(line), py_fp) != NULL && i<num) {
+        
+    }
+    else{
+        perror("No mcq's available for this programming language\n");
+        exit(1);
+    }
+
+    fclose(fp);
+    return ids;
+}
+
+/*
+* Returns a list of question ids
+* The seed is the unique int that is associated with the string
+*/
+int*question_ids(char prog_lang,int num,int seed){
+
+    int ids[num];
+    char *filename;
+
+    if(prog_lang != 'c' || prog_lang != 'p'){
+        error("QB does not exist");
+        exit(1);
+    }
+
+    //mitigate to different files or duplicate for each QB
+    if(prog_lang == 'c'){
+        filename = strndup("questionset_c.csv",18);
+    }
+    else if(prog_lang == 'p'){
+        filename = strndup("questionset_py.csv",20);
+    }
+
+    //generating question ids for 
+    srand(seed);
+    FILE *fp = fopen(filename,"r");
+    int upper = 10;//change according to number of available question in the respective QB
+    int lower = 0;
+
+    int i = 0;
+    while (i<num){
+        ids[i] = (rand() % (upper - lower + 1)) + lower;
+        ++i;
+    }
+
+    return ids;
+}
+
+
+/*
+* Returns a list of questions associated with question ids
+* The seed is the unique int that is associated with the string
+*/
+char**questions(int*ids){
+    while(fgets(line, sizeof(line), py_fp) != NULL && i<num) {
             sscanf(line,"%i,%c,%s",q_id,q_type,buff);
             printf("%s",buff);
             if(*q_type=='m'){
@@ -72,126 +131,9 @@ int*questions_mcq(char prog_lang, int num){
                 ++i;
             }
         }
-    }
-    else{
-        perror("No mcq's available for this programming language\n");
-        exit(1);
-    }
-    
-    return ids;
 }
 
-/*
-* Returns a list of coding question ids
-*/
-int*questions_cod(char prog_lang, int num){
-    int*ids=malloc(num*sizeof(int));
-
-    FILE *py_fp = fopen("questionset_py.csv","r");
-    FILE *c_fp = fopen("questionset_c.csv","r");
-
-    char line[500]; //no line is >499 chars
-    int i = 0;
-
-    //for both programming languages
-    FILE *fp;
-    if(prog_lang = 'a'){
-        while (i<num)
-        {
-            if(rand()%2==1) fp = py_fp;
-            else fp = c_fp;
-
-            int *q_id;
-            char *q_type;
-            char buff[200];
-            while(fgets(line, sizeof(line), fp) != NULL) {
-                sscanf(line,"%i,%c,%s",q_id,q_type,buff);
-                if(*q_type=='c'){
-                    ids[i]=*q_id;
-                    ++i;
-                }
-            }
-            
-        }
-    }
-    //for c
-    else if (prog_lang = 'c')
-    {
-        int *q_id;
-        char *q_type;
-        char buff[200];
-        while(fgets(line, sizeof(line), c_fp) != NULL && i<num) {
-            sscanf(line,"%i,%c,%s",q_id,q_type,buff);
-            if(*q_type=='c'){
-                ids[i]=*q_id;
-                ++i;
-            }
-        }
-    }
-    //for python
-    else if (prog_lang = 'p')
-    {
-        int *q_id;
-        char *q_type;
-        char buff[200];
-        while(fgets(line, sizeof(line), py_fp) != NULL && i<num) {
-            sscanf(line,"%i,%c,%s",q_id,q_type,buff);
-            if(*q_type=='c'){
-                ids[i]=*q_id;
-                ++i;
-            }
-        }
-    }
-    else{
-        perror("No coding questions available for this programming language\n");
-        exit(1);
-    }
-    
-    return ids;
-}
-
-/*
-* Returns a list of question ids
-*/
-int*question_ids(char prog_lang,int mcq, int cod, bool randomise){
-    int num = mcq+cod;
-    int*ids = malloc(num*sizeof(int));
-    int*cod_ids = malloc(cod*sizeof(int));
-
-    ids = questions_mcq(prog_lang,mcq);
-    cod_ids = questions_cod(prog_lang,cod);
-
-    for (int i=0;i<cod;i++){
-        ids[mcq+i]=cod_ids[i];
-    }
-
-    if(randomise){
-        // Ben Pfaff's 04 April 2004 list shuffling algorithm
-        // https://benpfaff.org/writings/clc/shuffle.html
-        size_t i;
-        for (i = 0; i < num - 1; i++) 
-        {
-          size_t j = i + rand() / (RAND_MAX / (num - i) + 1);
-          int t = ids[j];
-          ids[j] = ids[i];
-          ids[i] = t;
-        }
-    }
-    return ids;
-}
 
 int main(){
-    int mcq = 3;
-    int cod = 2;
-    char prog_lang = 'p';
-    bool random = true;
-
-    int num = mcq + cod;//for testing only
-    int*ques_ids;
-    ques_ids = malloc(num*sizeof(int));
-    ques_ids = question_ids(prog_lang,mcq,cod,random);
-
-    for (int i=0;i<num;i++){
-        printf("%i",ques_ids[i]);
-    }
+    
 }
