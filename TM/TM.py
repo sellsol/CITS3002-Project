405ecb06b20b2e11eec2d38b7bfff204668466df
sellsol <leowheidi@gmail.com>
2023-05-12 11:52:30 +0800

    python sockets 2

---
+++ TM/TM.py
@@ -0,0 +1,7 @@
+from config import *
+#TBC - imports
+
+if __name__ == "__main__":
+    pass #TBC
+
+    #TO-DO: 
---
+++ TM/config.py
@@ -0,0 +1,21 @@
+# bounds for TM
+num_questions = 3
+num_qbs = 2
+
+# TM databases
+loginFile = "test_login_file.txt"
+DB_filepath = "test_TM_database_v2.txt"
+
+# HTTP server address
+hostName = "localhost"
+serverPort = 8000
+
+# sockets server address
+HOST = "127.0.0.1"
+PORT = 65432
+
+# information for sockets
+#TBC
+
+#temp - TBC
+QB_portnums = [1401,2002] #ignore what this means for now
