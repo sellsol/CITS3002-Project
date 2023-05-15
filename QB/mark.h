13deb64fc605388f5d7f6996c00e932668e61fd0
developing-ar <noelis.roy@gmail.com>
2023-05-15 21:40:46 +0800

    header mark.c with: questions, mode, pquestions

---
+++ QB/mark.h
@@ -0,0 +1,7 @@
+#include "questions.h"
+#include "mode.h"
+#include "pquestions.h"
+
+extern bool question_correct(char*,char,int,int,int,char*);
+extern char *PY_A; //ans for python
+extern char *C_A; //ans for c
